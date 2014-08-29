68d1a53d9536198f1edc124bc10a2b70e93a73c6
Eric Smekens <ericsmekens@msn.com>
2014-08-29 22:31:18 +0200

    First basic simple server

---
+++ server.js
@@ -0,0 +1,35 @@
+var net = require('net'),
+    _ = require('underscore');
+
+var HOST = '127.0.0.1',
+    PORT = 8444;
+
+var players = [];
+
+// Create a server instance, and chain the listen function to it
+// The function passed to net.createServer() becomes the event handler for the 'connection' event
+// The sock object the callback function receives UNIQUE for each connection
+net.createServer(function (sock) {
+    players.push(sock);
+    // We have a connection - a socket object is assigned to the connection automatically
+    console.log('CONNECTED: ' + sock.remoteAddress + ':' + sock.remotePort);
+
+    // Add a 'data' event handler to this instance of socket
+    sock.on('data', function (data) {
+        console.log('DATA ' + sock.remoteAddress + ': ' + data);
+        players.forEach(function (player) {
+            if (player != sock) {
+                player.write('DATA ' + sock.remoteAddress + ': ' + data);
+            }
+        });
+    });
+
+    // Add a 'close' event handler to this instance of socket
+    sock.on('close', function (data) {
+        console.log('CLOSED: ' + sock.remoteAddress + ' ' + sock.remotePort);
+        players = _.reject(players, function(element) { return element == sock; });
+    });
+
+}).listen(PORT, HOST);
+
+console.log('Server listening on ' + HOST + ':' + PORT);
